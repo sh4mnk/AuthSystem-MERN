dc205f52ed2a4a2cb880e6d3af6f71295a76aee9
shaxmem <shameemshamem297@gmail.com>
2026-06-01 14:52:56 +0530

    debug the url

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,10 +1,12 @@
+import axios from "axios";
 
-import axios from "axios"
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
-  withCredentials: true
-})
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:5000/api",
+  withCredentials: true,
+});
 
-
-export default api
+export default api;
