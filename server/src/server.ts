32a751a4ec2042c601e3a1f97ebf6e909b758b21
shaxmem <shameemshamem297@gmail.com>
2026-06-04 01:31:24 +0530

    frontend url next

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,10 +9,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "auth-system-mern-eta.vercel.app",
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
