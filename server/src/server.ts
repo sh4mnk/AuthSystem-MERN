beb1733bd1d9a239adbdb9a3e1d71a024c6e860e
shaxmem <shameemshamem297@gmail.com>
2026-06-04 01:35:09 +0530

    http set normally

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "auth-system-mern-eta.vercel.app",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
